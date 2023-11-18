91b9bfafb296f4cd1a0f99afed09ee937422f824
guventuncay <guven.tuncay01@gmail.com>
2023-11-18 18:16:44 +0300

    leetcode - 125. Valid Palindrome

--- src/leetcode/easy_125_valid_palindrome/Solution.java
+++ src/leetcode/easy_125_valid_palindrome/Solution.java
@@ -1,22 +1,16 @@
 package leetcode.easy_125_valid_palindrome;
 
 class Solution {
-
-
     public static void main(String[] args) {
         String i1 = "A man, a plan, a canal: Panama";
         boolean e1 = true;
         boolean a1 = isPalindrome(i1);
         System.out.println("(actual == expected) = " + (a1 == e1));
-
         String i2 = "0P";
         boolean e2 = false;
         boolean a2 = isPalindrome(i2);
         System.out.println("(actual == expected) = " + (a2 == e2));
-
-
     }
-
 
     public static boolean isPalindrome(String str) {
         int left = 0;
