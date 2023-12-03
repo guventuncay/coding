b369f7a54d63fbc77ec979653f9773e58cd3dc9b
guventuncay <guven.tuncay01@gmail.com>
2023-12-03 08:52:51 +0300

    codewars - Convert string to camel case

---
+++ src/codewars/Convert string to camel case.java
@@ -0,0 +1,34 @@
+package codewars;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Solution {
+
+    static String toCamelCase(String s) {
+        String[] split = s.split("[_-]");
+        return Arrays.stream(split)
+                .reduce((a, b) -> a.concat(b.substring(0, 1).toUpperCase().concat(b.substring(1))))
+                .get();
+    }
+
+    static class SolutionTest {
+        @Test
+        public void testSomeUnderscoreLowerStart() {
+            String input = "the_Stealth_Warrior";
+            System.out.println("input: " + input);
+            assertEquals("theStealthWarrior", Solution.toCamelCase(input));
+        }
+
+        @Test
+        public void testSomeDashLowerStart() {
+            String input = "the-Stealth-Warrior";
+            System.out.println("input: " + input);
+            assertEquals("theStealthWarrior", Solution.toCamelCase(input));
+        }
+    }
+}
+
